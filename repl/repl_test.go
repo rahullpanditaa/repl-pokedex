dfa40139c961beca71970150cce6a7e77c0f5cd5
Rahul Pandita <rpandita1999@outlook.com>
2025-07-04 02:22:18 +0000

    create more tests for cleanInput function

--- repl/repl_test.go
+++ repl/repl_test.go
@@ -6,11 +6,32 @@ import (
 )
 
 func TestCleanInput(t *testing.T) {
-	inputString := "hello world"
-	got := cleanInput(inputString)
-	want := []string{"hello", "world"}
+	t.Run("uppercase words", func(t *testing.T) {
+		inputString := "ThIs teXT has some UPPERCASE leTTERS"
+		got := cleanInput(inputString)
+		want := []string{"this", "text", "has", "some", "uppercase", "letters"}
 
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v given, %s", got, want, inputString)
-	}
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v given, %s", got, want, inputString)
+		}
+	})
+	t.Run("sanitized text", func(t *testing.T) {
+		inputString := "hello world"
+		got := cleanInput(inputString)
+		want := []string{"hello", "world"}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v given, %s", got, want, inputString)
+		}
+	})
+	t.Run("uppercase and extra whitespace", func(t *testing.T) {
+		inputString := "   This TEXT has leading And trailing whitespace   "
+		got := cleanInput(inputString)
+		want := []string{"this", "text", "has", "leading", "and", "trailing", "whitespace"}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v given, %s", got, want, inputString)
+		}
+	})
+
 }
